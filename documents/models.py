be9dda0b02415551c1354d679e0a4b6a11b94132
automate-admin <ticketit2026@gmail.com>
2026-09-07 18:08:25 +0330

    Update models.py

--- documents/models.py
+++ documents/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import User
+from django.contrib.auth.models import User
 
 class Document(models.Model):
     title = models.CharField(max_length=200)
