6e9aed9291f5134579bc3b5712b7c184669f31ec
automate-admin <ticketit2026@gmail.com>
2026-09-08 21:59:37 +0330

    Update settings.py

--- automate_admin/settings.py
+++ automate_admin/settings.py
@@ -58,10 +58,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'automate_admin.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
