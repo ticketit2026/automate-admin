220231e4ebddef338e8e9e41c288d8951775ed80
automate-admin <ticketit2026@gmail.com>
2026-09-08 20:48:30 +0330

    Update views.py

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Ticket
 
 
 def ticket_list(request):
+
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        priority = request.POST.get('priority')
+
+        Ticket.objects.create(
+            title=title,
+            description=description,
+            priority=priority,
+            created_by=request.user
+        )
+
+        return redirect('ticket_list')
+
     tickets = Ticket.objects.all().order_by('-created_at')
 
     return render(
