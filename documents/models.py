d1749a59143414c112110677e846f5c7a2e9cdff
automate-admin <ticketit2026@gmail.com>
2026-09-08 20:07:58 +0330

    Update models.py

--- documents/models.py
+++ documents/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Document(models.Model):
     title = models.CharField(max_length=200)
     file = models.FileField(upload_to='documents/')
