1a9dd3826fa705dbb32e2a73887a093574766807
automate-admin <ticketit2026@gmail.com>
2026-09-08 20:12:44 +0330

    Update views.py

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -5,6 +5,10 @@ from .models import Ticket
 def ticket_list(request):
     tickets = Ticket.objects.all().order_by('-created_at')
 
-    return render(request, 'helpdesk/ticket_list.html', {
-        'tickets': tickets
-    })
+    return render(
+        request,
+        'helpdesk/ticket_list.html',
+        {
+            'tickets': tickets
+        }
+    )
