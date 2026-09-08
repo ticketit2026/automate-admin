288153d14c76e638971475e100b2e859e8544296
automate-admin <ticketit2026@gmail.com>
2026-09-08 21:09:14 +0330

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),   # اگر بخوای با Django auth
-    # می‌تونی بعداً با allauth اضافه کنی
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='registration/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
 ]
