274830334438280764b1299f744b69cd48ca8ee6
automate-admin <ticketit2026@gmail.com>
2026-09-08 19:39:25 +0330

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from helpdesk.models import Ticket
 
 
 def overview(request):
-    return render(request, 'dashboard/overview.html')
+    tickets_count = Ticket.objects.count()
+
+    return render(
+        request,
+        'dashboard/overview.html',
+        {
+            'tickets_count': tickets_count
+        }
+    )
