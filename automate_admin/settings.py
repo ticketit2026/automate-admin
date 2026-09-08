8b485ff5b8f2141a28012f0f690da4eba826e6db
automate-admin <ticketit2026@gmail.com>
2026-09-08 21:11:29 +0330

    Update settings.py

--- automate_admin/settings.py
+++ automate_admin/settings.py
@@ -81,3 +81,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/user/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGOUT_REDIRECT_URL = '/user/login/'
+
