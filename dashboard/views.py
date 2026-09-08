1bab94b52e5d6ee7012afa9d06b05339d20b6139
automate-admin <ticketit2026@gmail.com>
2026-09-08 19:22:27 +0330

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 
+from helpdesk.models import Ticket
+from admin_automation.models import InternalLetter
+from workflow.models import LeaveRequest, PurchaseRequest, MissionRequest
+from meetings.models import Meeting
+
 
 def overview(request):
-    return render(request, 'dashboard/overview.html')
+
+    tickets_count = Ticket.objects.count()
+
+    letters_count = InternalLetter.objects.count()
+
+    pending_requests = (
+        LeaveRequest.objects.filter(status='pending').count()
+        + PurchaseRequest.objects.filter(status='pending').count()
+        + MissionRequest.objects.filter(status='pending').count()
+    )
+
+    meetings_count = Meeting.objects.count()
+
+    context = {
+        'tickets_count': tickets_count,
+        'letters_count': letters_count,
+        'pending_requests': pending_requests,
+        'meetings_count': meetings_count,
+    }
+
+    return render(
+        request,
+        'dashboard/overview.html',
+        context
+    )
