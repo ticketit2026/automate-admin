c8648fa626afc7ef698185c261b6b90d4c1862ab
automate-admin <ticketit2026@gmail.com>
2026-09-08 21:01:57 +0330

    Update views.py

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Ticket
 
 
+@login_required
 def ticket_list(request):
 
     if request.method == 'POST':
