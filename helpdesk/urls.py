4c60791860d70914f4d52ec823a0dfa25d59c222
automate-admin <ticketit2026@gmail.com>
2026-09-09 18:31:54 +0330

    Update urls.py

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.ticket_list, name='ticket_list'),
     path('<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
+    path(
+        '<int:ticket_id>/status/',
+        views.update_ticket_status,
+        name='update_ticket_status'
+    ),
 ]
