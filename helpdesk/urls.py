923e2bc836b35690f8e66a829225270d99303fc7
automate-admin <ticketit2026@gmail.com>
2026-09-09 18:44:44 +0330

    Update urls.py

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.ticket_list, name='ticket_list'),
-    path('<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
+
+    path(
+        '',
+        views.ticket_list,
+        name='ticket_list'
+    ),
+
+    path(
+        '<int:ticket_id>/',
+        views.ticket_detail,
+        name='ticket_detail'
+    ),
+
     path(
         '<int:ticket_id>/status/',
         views.update_ticket_status,
         name='update_ticket_status'
     ),
+
+    path(
+        '<int:ticket_id>/assign/',
+        views.assign_ticket,
+        name='assign_ticket'
+    ),
+
 ]
