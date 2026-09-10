3d4523066094cc79ae5055c059e3ad0ed19918ed
automate-admin <ticketit2026@gmail.com>
2026-09-10 17:43:36 +0330

    Update settings.py

--- automate_admin/settings.py
+++ automate_admin/settings.py
@@ -60,7 +60,6 @@ WSGI_APPLICATION = 'automate_admin.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
         conn_max_age=600
     )
 }
