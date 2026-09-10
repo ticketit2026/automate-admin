49aa52793de35ca3597ec086a350cd5e9adaa981
automate-admin <ticketit2026@gmail.com>
2026-09-10 20:11:41 +0330

    Update views.py

--- user_management/views.py
+++ user_management/views.py
@@ -1,17 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 
 @login_required
 def user_list(request):
-
-    users = User.objects.all().order_by('username')
-
-    return render(
-        request,
-        'user_management/user_list.html',
-        {
-            'users': users
-        }
-    )
+    return HttpResponse("User Management OK")
