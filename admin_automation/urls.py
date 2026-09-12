706c489ca529ae015be2b7dafed72715a6f3c6dc
automate-admin <ticketit2026@gmail.com>
2026-09-12 18:49:00 +0330

    Update urls.py

--- admin_automation/urls.py
+++ admin_automation/urls.py
@@ -1,6 +1,44 @@
 from django.urls import path
+
 from . import views
 
+
 urlpatterns = [
-    path('', views.letter_list, name='letter_list'),
+
+    path(
+        '',
+        views.letter_list,
+        name='letter_list'
+    ),
+
+    path(
+        'create/',
+        views.letter_create,
+        name='letter_create'
+    ),
+
+    path(
+        '<int:letter_id>/',
+        views.letter_detail,
+        name='letter_detail'
+    ),
+
+    path(
+        '<int:letter_id>/edit/',
+        views.letter_edit,
+        name='letter_edit'
+    ),
+
+    path(
+        '<int:letter_id>/delete/',
+        views.letter_delete,
+        name='letter_delete'
+    ),
+
+    path(
+        '<int:letter_id>/send/',
+        views.letter_send,
+        name='letter_send'
+    ),
+
 ]
